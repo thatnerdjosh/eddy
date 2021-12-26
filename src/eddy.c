2371dd81e54c943ad33b073233289cfc8ef26ae9
Doug Yanez <Deepspeed@Hacker.Deepspeed@gmail.com>
2021-12-26 10:03:38 -0600

    minor changes

--- src/eddy.c
+++ src/eddy.c
@@ -1,67 +1,66 @@
 //  gcc -o eddy eddy.c `pkg-config --cflags --libs eina efl elementary`
 
 /* TODO:
- * Add callback for ok button to initialize `dd` function
- * Write dd function
- * callback for cancel button */
+ * add md5 checker button and make it work
+ * add USB drive selector and checker
+ * add ISO installer button using dd command */
 
 #include <Elementary.h>
 
 
 //these functions are example code only.  Needs updating to be useful.
 
-/* Need to use this one to get the ISO selected and set it to a visible entry*/
+/* get the ISO selected and set it to a visible entry*/
 static void 
 iso_chosen(void *data, Evas_Object *obj EINA_UNUSED, void *event_info)
 {
+/*need to add error handling in case a directory is selected*/
 	const char *file = event_info;
 	Evas_Object *entry = data;
 	elm_object_text_set(entry, file);
 	
-	printf("File Chosen: %s\n",
-          file ? file : "*none chosen!*");
+	printf("File Chosen: %s\n", file ? file : "*none chosen!*");
     
 }
 
 
 /*Get md5sum file selected and set it to another visible entry*/
-/*FIX ME: The **first time** cancel is selected it performs ok's actions. Every other times behaves as expected*/
 
 static void 
-md5_chosen(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info)
+md5_chosen(void *data EINA_UNUSED,Evas_Object *obj EINA_UNUSED,void *event_info)
 {
+/*need to add error handling in case a directory is selected*/
 	const char *file = event_info;
 	Evas_Object *entry = data;
 	elm_object_text_set(entry, file);
 	
-	printf("File Chosen: %s\n",
-          file ? file : "*none Chosen!*");
+	printf("File Chosen: %s\n", file ? file : "*none Chosen!*");
 }
 
-// Functions for ok (`dd` stuff) and cancel buttons callback
 
 
 
 EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
-	Evas_Object *win, *grid, *hbox, *ic1, *ic2;
-	Evas_Object *iso_bt, *md5_bt, *entry1, *entry2, *sep;
+	Evas_Object *win, *grid, *hbox, *ic1, *ic2, *entry1, *entry2, *sep;
+	Evas_Object *iso_bt, *md5_bt, *md5_check_bt, *iso_check_bt, *dd_bt;
+	//still need a few labels or icons later.
 
 	elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
 
+	//set up window
 	win = elm_win_util_standard_add("main", "Eddy - Live USB Utility");
 	elm_win_autodel_set(win, EINA_TRUE);
 
+	//make grid
 	grid = elm_grid_add(win);
 	evas_object_size_hint_weight_set(grid,EVAS_HINT_EXPAND,EVAS_HINT_EXPAND);
 	elm_win_resize_object_add(win, grid);
 	evas_object_show(grid);
 
-	ic1 = elm_icon_add(win);
-	elm_icon_standard_set(ic1, "file");
-	evas_object_size_hint_aspect_set(ic1,EVAS_ASPECT_CONTROL_HORIZONTAL,1,1);
-	
-	
+	/*need to add filters to the fileselectors so they only show the
+	* relevant file types for each button*/
+		
 	/* MD5 selector button */
 	ic2 = elm_icon_add(win);
 	elm_icon_standard_set(ic2, "file");
@@ -74,18 +73,23 @@ EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 	elm_fileselector_folder_only_set(md5_bt, EINA_FALSE);
 	elm_fileselector_is_save_set(md5_bt, EINA_TRUE);
 	elm_object_text_set(md5_bt, "Select MD5");
-	evas_object_resize(md5_bt, 100, 30);
+	evas_object_resize(md5_bt, 100, 30);//not sure if this helps
 	elm_object_part_content_set(md5_bt, "icon", ic2);
 
-	elm_grid_pack_set(md5_bt,3,3,22,8);
+	elm_grid_pack_set(md5_bt,3,3,22,8);//this sets size, not evas resize?
 	evas_object_show(md5_bt);
 	evas_object_show(ic2);
+	
+	//are these icons doing anything??
+	ic1 = elm_icon_add(win);
+	elm_icon_standard_set(ic1, "file");
+	evas_object_size_hint_aspect_set(ic1,EVAS_ASPECT_CONTROL_HORIZONTAL,1,1);
 	
 	/* MD5 text entry */
 	entry2 = elm_entry_add(grid);
 	elm_entry_line_wrap_set(entry2, EINA_FALSE);
 	elm_entry_editable_set(entry2, EINA_FALSE);
-	evas_object_size_hint_min_set(entry2,140,25);// The hint isn't enforced just used where appropriate. What are we trying to do?
+	evas_object_size_hint_min_set(entry2,140,25);
 	elm_grid_pack_set(entry2,30,3,42,8);
 	evas_object_show(entry2);
 
@@ -112,8 +116,9 @@ EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 	evas_object_size_hint_min_set(entry1,140,25);
 	elm_grid_pack_set(entry1,30,11,42,8);
 	evas_object_show(entry1);
+
 	
-		// separator line
+	// separator line
 	sep = elm_separator_add(win);
 	elm_separator_horizontal_set(sep, EINA_TRUE);
 	elm_grid_pack(grid,sep,1,60,100,1);
@@ -121,11 +126,11 @@ EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 	
 	
 	//add callbacks for buttons
-	evas_object_smart_callback_add(iso_bt, "file,chosen", iso_chosen, entry1);
-	evas_object_smart_callback_add(md5_bt, "file,chosen", md5_chosen, entry2);
+	evas_object_smart_callback_add(iso_bt,"file,chosen",iso_chosen, entry1);
+	evas_object_smart_callback_add(md5_bt,"file,chosen",md5_chosen, entry2);
 	
 	evas_object_resize(win, 430, 340);
-	//   evas_object_show(grid);
+	//evas_object_show(grid);
 	evas_object_show(win);
 
 	elm_run();
