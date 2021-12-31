2e89f9ff540185f3be0dc0fa4231f4cb05fc9642
Doug Yanez <Deepspeed@Hacker.Deepspeed@gmail.com>
2021-12-30 19:33:04 -0600

    update md5 checker and gui

--- src/eddy.c
+++ src/eddy.c
@@ -19,7 +19,7 @@
  *
  * Change popen() to ecore_exe
  *
- * Get progress bar working
+ * Get progress bar workinghttps://github.com/Deepspeed/eddy
  *
  * add USB drive selector and checker
  *
@@ -35,7 +35,7 @@
 
 
 
-Evas_Object *entry2, *pb;
+Evas_Object *entry2, *lb2, *pb;
 
 
 /* get the ISO selected and set it to a visible entry*/
@@ -44,7 +44,8 @@ iso_chosen(void *data, Evas_Object *obj EINA_UNUSED, void *event_info)
 {
 
 	const char *file = event_info;
-	Evas_Object *entry = data;
+	char buf[PATH_MAX];
+	Evas_Object *lb1 = data;
 	
 	if(!file)
 		return;
@@ -55,7 +56,8 @@ iso_chosen(void *data, Evas_Object *obj EINA_UNUSED, void *event_info)
 		printf("Wrong file type!  Try again.\n");
 		return;
 	}
-	elm_object_text_set(entry, file);
+	snprintf(buf, sizeof(buf), "<align=left>%s", file);
+	elm_object_text_set(lb1, buf);
 	
 	//printf("File Chosen: %s\n", file ? file : "*none chosen!*");    
 }
@@ -77,20 +79,35 @@ md5_check(void *data, Evas_Object *o EINA_UNUSED, void *e)
 	FILE *ptr; //Our favorite pointer.  His name is Peter.
 	
 	char ch;
+	//filepaths
+	char isoPath[PATH_MAX];
+	char md5Path[PATH_MAX];
 	char folderPath[PATH_MAX];
-	char output[PATH_MAX];
-	char command[PATH_MAX];
-	//char result[PATH_MAX];
-
-	const char *isoPath = elm_object_text_get(data);
-
+	char command[PATH_MAX];//terminal command
+	char output[PATH_MAX];//command output
+	char result[PATH_MAX];//final parsed result
 	
+	char tmp[PATH_MAX];
+	char str[] = "<align=left>Md5sum checked: ";
+
+	const char *tmpPath = elm_object_text_get(data);
+	
+	
+	/* remove extra <align=left> bit */
+	tmp[0] = '\0';
+	strcat(tmp,tmpPath);
+	for (i=0; tmp[i+12] != '\0'; i++)
+		isoPath[i] = tmp[i+12];
+	
+	isoPath[i+1] = '\0';
+	
+	//error handling
 	if(!*isoPath) {
 		printf("No .iso file chosen yet!\n");
 		return;
 	}
 	
-
+	//activate progress bar (it will work one day)
 	elm_progressbar_pulse(pb,EINA_TRUE);
 	elm_object_text_set(entry2, "Checking ISO md5sum");
 	
@@ -106,6 +123,8 @@ md5_check(void *data, Evas_Object *o EINA_UNUSED, void *e)
 		break;
 	}
 	
+	//this creates overflow warnings
+//	snprintf(command, sizeof(command),"%s%s%s%s%s","cd ", folderPath, " && md5sum -c ", isoPath, ".md5");
 	
 	//build terminal command
 	command[0] == '\0';
@@ -115,9 +134,22 @@ md5_check(void *data, Evas_Object *o EINA_UNUSED, void *e)
 	strcat(command, isoPath);
 	strcat(command, ".md5");
 	
+	//build md5 file path
+	md5Path[0] = '\0';
+	strcpy(md5Path,isoPath);
+	strcat(md5Path,".md5");
+
+	//check for md5 file existence
+	if(!ecore_file_exists(md5Path)){
+		printf("md5 file does not exist in this folder!\n");
+		return;
+	}
 	
-// need to verify md5 file exists
-	
+	//readability check
+	if(!ecore_file_can_read(md5Path)){
+		printf("md5 file cannot be read!\n");
+		return;
+	}
 	
 
 	/*execute md5 check.  
@@ -144,8 +176,20 @@ md5_check(void *data, Evas_Object *o EINA_UNUSED, void *e)
 	output[i+1] = '\0'; //safety pads
 	//printf("%s\n", output);
 	pclose(ptr); //Let Peter rest.  He worked hard today.
+	
+	while(output[i] != ':')
+		i--;
+	i += 2;
+	while(output[i] != '\0'){
+		result[j] = output[i];
+		i++;
+		j++;
+	}
+	result[j] = '\0';  //safety pad
+	
+	strcat(str, result);
 		
-	elm_object_text_set(entry2, output);//change to 'result' if needed
+	elm_object_text_set(lb2, str);//change to 'result' if needed
 
 	//stop progress bar...  none of this works.
 	elm_progressbar_pulse(pb, EINA_FALSE);
@@ -244,10 +288,10 @@ gl_filter_get(void *data, Evas_Object *obj EINA_UNUSED, void *key)
 
 
 
-/* remove grid and replace with table and frames */
+/* remove entries and replace with labels */
 EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 {
-	Evas_Object *win, *table, *hbox, *ic1, *ic2, *entry1, *entry3;
+	Evas_Object *win, *table, *hbox, *ic1, *ic2, *entry1, *entry3, *lb1, *lb3;
 	Evas_Object *iso_bt, *md5_check_bt, *usb_check_bt, *dd_bt;
 	Evas_Object *help_bt, *sep, *combo;
 	Elm_Genlist_Item_Class *glist;
@@ -274,64 +318,49 @@ EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 	/* ISO selector button */
 
 	iso_bt = elm_fileselector_button_add(table);
-//	elm_fileselector_path_set(iso_bt, "/home/");
 	elm_fileselector_button_inwin_mode_set(iso_bt, EINA_TRUE);
 	elm_fileselector_expandable_set(iso_bt, EINA_FALSE);
 	elm_fileselector_folder_only_set(iso_bt, EINA_FALSE);
 	elm_fileselector_is_save_set(iso_bt, EINA_TRUE);
 	elm_object_text_set(iso_bt, "Select ISO");
-//	efl_gfx_hint_align_set(iso_bt, -1, 0.5);
 	evas_object_size_hint_align_set(iso_bt, EVAS_HINT_FILL, 0.5);
 
 	elm_table_pack(table,iso_bt,0,0,1,1);
 	evas_object_show(iso_bt);
-
-	/* ISO text entry */
-	entry1 = elm_entry_add(table);
-//	elm_entry_line_wrap_set(entry1, EINA_FALSE);
-	elm_entry_editable_set(entry1, EINA_FALSE);
-//	efl_gfx_hint_align_set(entry1, -1, 0.5);
-
-	elm_entry_scrollable_set(entry1,EINA_TRUE);
-	elm_scroller_policy_set(entry2,ELM_SCROLLER_POLICY_OFF,ELM_SCROLLER_POLICY_OFF);
-	elm_entry_single_line_set(entry1, EINA_TRUE);
-	evas_object_size_hint_weight_set(entry1, EVAS_HINT_EXPAND, 0.0);
-	evas_object_size_hint_align_set(entry1, EVAS_HINT_FILL, 0.5);
-
-	elm_table_pack(table,entry1,1,0,2,1);
-	evas_object_show(entry1);
-
 	
+	
+	/* ISO text label */
+	lb1 = elm_label_add(table);
+	elm_label_ellipsis_set(lb1, EINA_TRUE);
+	evas_object_size_hint_weight_set(lb1, EVAS_HINT_EXPAND, 0.0);
+	evas_object_size_hint_align_set(lb1, EVAS_HINT_FILL, 0.0);
+	
+	elm_table_pack(table, lb1, 1,0,5,1);
+	evas_object_show(lb1);
+	
+
 	/* Md5 Check Button */
 	md5_check_bt = elm_button_add(table);
 	elm_button_autorepeat_set(md5_check_bt, EINA_FALSE);
 	elm_object_text_set(md5_check_bt, "Check md5");
-//	efl_gfx_hint_align_set(md5_check_bt, -1, 0.5);
 	evas_object_size_hint_align_set(md5_check_bt, EVAS_HINT_FILL, 0.5);
 
 	elm_table_pack(table,md5_check_bt,0,1,1,1);
 	evas_object_show(md5_check_bt);
 
-	/* md5 result entry */
-	entry2 = elm_entry_add(table);
-//	elm_entry_line_wrap_set(entry2, EINA_FALSE);
-	elm_entry_editable_set(entry2, EINA_FALSE);
-//	efl_gfx_hint_align_set(entry2, -2, 0.5);
-
-	elm_entry_scrollable_set(entry2, EINA_TRUE);
-	elm_scroller_policy_set(entry2,ELM_SCROLLER_POLICY_OFF,ELM_SCROLLER_POLICY_OFF);
-	elm_entry_single_line_set(entry2, EINA_TRUE);
-	evas_object_size_hint_weight_set(entry2, EVAS_HINT_EXPAND, 0.0);
-	evas_object_size_hint_align_set(entry2, EVAS_HINT_FILL, 0.5);
-
-	elm_table_pack(table,entry2,1,1,2,1);
-	evas_object_show(entry2);
+	/* md5 text label */
+	lb2 = elm_label_add(table);
+	elm_label_ellipsis_set(lb2, EINA_TRUE);
+	evas_object_size_hint_weight_set(lb2, EVAS_HINT_EXPAND, 1.0);
+	evas_object_size_hint_align_set(lb2, EVAS_HINT_FILL, 0.0);
+	
+	elm_table_pack(table, lb2, 1,1,5,1);
+	evas_object_show(lb2);
 
 	
 	/* USB chooser box */
 	combo = elm_combobox_add(table);
 	elm_object_part_text_set(combo, "Chooser", "Chose USB Drive");
-//	efl_gfx_hint_align_set(combo, 0.9, 0.5);
 	evas_object_size_hint_weight_set(combo, EVAS_HINT_EXPAND, 0.0);
 	evas_object_size_hint_align_set(combo, EVAS_HINT_FILL, 0.5);
 
@@ -359,45 +388,36 @@ EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 	usb_check_bt = elm_button_add(table);
 	elm_button_autorepeat_set(usb_check_bt, EINA_FALSE);
 	elm_object_text_set(usb_check_bt, "Check USB");
-//	efl_gfx_hint_align_set(usb_check_bt, -1, 0.5);
 	evas_object_size_hint_align_set(usb_check_bt, EVAS_HINT_FILL, 0.5);
 
 	elm_table_pack(table,usb_check_bt,0,3,1,1);
 	evas_object_show(usb_check_bt);
 	
-	//add label3 here for usb check results
-	/* usb check result entry */
-	entry3 = elm_entry_add(table);
-//	elm_entry_line_wrap_set(entry3, EINA_FALSE);
-	elm_entry_editable_set(entry3, EINA_FALSE);
-	elm_entry_scrollable_set(entry3, EINA_TRUE);
-	elm_scroller_policy_set(entry3, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_OFF);
-	elm_entry_single_line_set(entry3, EINA_TRUE);
-	evas_object_size_hint_weight_set(entry3, EVAS_HINT_EXPAND, 0.0);
-	evas_object_size_hint_align_set(entry3, EVAS_HINT_FILL, 0.5);
-//	efl_gfx_hint_align_set(entry3, -1, 0.5);
-
-	elm_table_pack(table,entry3,1,3,2,1);
-	evas_object_show(entry3);
+	/* USB result label */
+	lb3 = elm_label_add(table);
+	elm_label_ellipsis_set(lb3, EINA_TRUE);
+	evas_object_size_hint_weight_set(lb3, EVAS_HINT_EXPAND, 0.0);
+	evas_object_size_hint_align_set(lb3, EVAS_HINT_FILL, 0.5);
 	
+	elm_table_pack(table, lb3, 1,3,5,1);
+	evas_object_show(lb3);
+
 	/* DD buttom */
 	dd_bt = elm_button_add(table);
 	elm_button_autorepeat_set(dd_bt, EINA_FALSE);
 	elm_object_text_set(dd_bt, "Make Live USB");
-//	efl_gfx_hint_align_set(dd_bt, -1, 0.5);
 	evas_object_size_hint_align_set(dd_bt, EVAS_HINT_FILL, 0.5);
 
-	elm_table_pack(table,dd_bt,0,10,1,1);
+	elm_table_pack(table,dd_bt,0,5,1,1);
 	evas_object_show(dd_bt);
 
 	/* help button */
 	help_bt = elm_button_add(table);
 	elm_button_autorepeat_set(help_bt, EINA_FALSE);
 	elm_object_text_set(help_bt, "Help");
-//	efl_gfx_hint_align_set(help_bt, -1, 0.5);
 	evas_object_size_hint_align_set(help_bt, EVAS_HINT_FILL, 0.5);
 	
-	elm_table_pack(table,help_bt,34,13,1,1);
+	elm_table_pack(table,help_bt,5,6,1,1);
 	evas_object_show(help_bt);
 	
 	/* progress bar */
@@ -408,24 +428,23 @@ EAPI_MAIN int elm_main(int argc EINA_UNUSED, char **argv EINA_UNUSED)
 	evas_object_size_hint_weight_set(pb, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	elm_progressbar_pulse_set(pb, EINA_TRUE);
 	elm_progressbar_unit_format_set(pb, NULL);
-//	efl_gfx_hint_align_set(pb, 0.0, 0.5);
 	
 //	elm_progressbar_pulse(pb, EINA_TRUE);
-	elm_table_pack(table,pb,0,14,35,1);
+	elm_table_pack(table,pb,0,10,6,1);
 	evas_object_show(pb);
 	
 
 	
 	//add callbacks for buttons
-	evas_object_smart_callback_add(iso_bt,"file,chosen",iso_chosen, entry1);
-	evas_object_smart_callback_add(md5_check_bt,"clicked",md5_check,entry1);
+	evas_object_smart_callback_add(iso_bt,"file,chosen",iso_chosen, lb1);
+	evas_object_smart_callback_add(md5_check_bt,"clicked",md5_check,lb1);
 	evas_object_smart_callback_add(usb_check_bt,"clicked",usb_check,NULL);
 	evas_object_smart_callback_add(dd_bt,"clicked",make_usb,NULL);
 	evas_object_smart_callback_add(help_bt,"clicked",help_info,NULL);
 	
 	/* set final window size and display it */
 	
-	evas_object_resize(win, ELM_SCALE_SIZE(400), ELM_SCALE_SIZE(240));
+	evas_object_resize(win, ELM_SCALE_SIZE(420), ELM_SCALE_SIZE(300));
 	evas_object_show(table);  //is this needed?
 	evas_object_show(win);
 
