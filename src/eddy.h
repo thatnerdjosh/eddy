d96600af25d4f7b69bf9a0abfd1225f90a10fa0c
Robert Wiley <ylee@bodhilinux.com>
2022-01-09 07:27:20 -0500

    Add header file

---
+++ src/eddy.h
@@ -0,0 +1,29 @@
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE
+#endif
+
+#include "../config.h"
+#include <Elementary.h>
+#include <Eeze.h>
+#include <time.h>
+#include <string.h>
+
+#ifdef ENABLE_NLS
+# include <libintl.h>
+# define _(x) gettext(x)
+#else
+# define _(x) (x)
+#endif
+
+#define BUFFER_SIZE 1024
+
+int _eddy_log_dom;
+#define EDDY_DEFAULT_LOG_COLOR EINA_COLOR_CYAN
+
+#define CRI(...)      EINA_LOG_DOM_CRIT(_eddy_log_dom, __VA_ARGS__)
+#define ERR(...)      EINA_LOG_DOM_ERR(_eddy_log_dom, __VA_ARGS__)
+#define WRN(...)      EINA_LOG_DOM_WARN(_eddy_log_dom, __VA_ARGS__)
+#define INF(...)      EINA_LOG_DOM_INFO(_eddy_log_dom, __VA_ARGS__)
+#define DBG(...)      EINA_LOG_DOM_DBG(_eddy_log_dom, __VA_ARGS__)
+
+Eina_Bool debug=EINA_FALSE;
